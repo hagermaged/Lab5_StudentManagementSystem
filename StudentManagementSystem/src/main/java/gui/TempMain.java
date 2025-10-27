c25bb30a7f2f8b471450ed4a1d9101dc255637d2
JanaNafei <jananafei103@gmail.com>
2025-10-27 22:14:59 +0300

    all files

---
+++ StudentManagementSystem/src/main/java/gui/TempMain.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Hajer1
+ */
+package gui;
+
+import java.io.IOException;
+
+public class TempMain {
+    public static void main(String[] args) throws IOException {
+       // Debug the file first
+       // model.FileHandler.debugFile();
+        // Use your actual MainFrame instead of basic JFrame
+        MainFrame frame = new MainFrame();
+        frame.setVisible(true);
+    }
+}
